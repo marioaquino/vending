cc01cbdac3122145b922d16363adedec90b08e76
Mario Aquino <mario.e.aquino@gmail.com>
2009-04-03 23:40:48 -0500

    Another refactoring to introduce context in RSpec

--- spec/vending_mode_spec.rb
+++ spec/vending_mode_spec.rb
@@ -7,41 +7,47 @@ describe VendingMode do
   before(:each) do
     @vending = Object.new.extend(VendingMode)
   end
+  context "accepting money" do
+    it "should have an empty coin return if no money has been added" do
+      @vending.coin_return.should be_empty
+    end
   
-  it "should have an empty coin return if no money has been added" do
-    @vending.coin_return.should be_empty
+    it "should accept money and display amount added" do
+      @vending.add_money(QUARTER)
+      @vending.money_added.should == 25
+    end
+  
+    it "should correctly keep track of the amount of money added over successive adds" do
+      [DOLLAR, QUARTER, DIME, NICKEL].each {|coin| @vending.add_money coin}
+      @vending.money_added.should == 140
+    end
+  
+    it "should return money added when a sale is cancelled" do
+      [QUARTER, DIME ].each {|coin| @vending.add_money coin}
+      @vending.cancel
+      @vending.coin_return.should include(QUARTER, DIME)
+    end
+  
+    it "should show no money added after a sale is cancelled" do
+      @vending.add_money QUARTER
+      @vending.cancel
+      @vending.money_added.should == 0
+    end
   end
-  
-  it "should accept money and display amount added" do
-    @vending.add_money(QUARTER)
-    @vending.money_added.should == 25
+
+  context "during sales" do
+
+    before(:each) do
+      @purse = []
+    end
+    
+    it "should dispense an item from the A row if you deposit 65 cents and select A" do
+      @vending.should_receive(:purse).and_return(@purse)
+      @vending.should_receive(:dispense).with(:a).and_return(:Doritos)
+      [QUARTER, QUARTER, DIME, NICKEL].each {|coin| @vending.add_money coin}
+      @vending.select_a.should == :Doritos
+      @vending.money_added.should == 0
+      @purse.should include(QUARTER, QUARTER, DIME, NICKEL)
+    end
   end
-  
-  it "should correctly keep track of the amount of money added over successive adds" do
-    [DOLLAR, QUARTER, DIME, NICKEL].each {|coin| @vending.add_money coin}
-    @vending.money_added.should == 140
-  end
-  
-  it "should return money added when a sale is cancelled" do
-    [QUARTER, DIME ].each {|coin| @vending.add_money coin}
-    @vending.cancel
-    @vending.coin_return.should include(QUARTER, DIME)
-  end
-  
-  it "should show no money added after a sale is cancelled" do
-    @vending.add_money QUARTER
-    @vending.cancel
-    @vending.money_added.should == 0
-  end
-  
-  it "should dispense an item from the A row if you deposit 65 cents and select A" do
-    @vending.should_receive(:dispense).with(:a).and_return(:Doritos)
-    purse = []
-    @vending.should_receive(:purse).and_return(purse)
-    [QUARTER, QUARTER, DIME, NICKEL].each {|coin| @vending.add_money coin}
-    @vending.select_a.should == :Doritos
-    @vending.money_added.should == 0
-    purse.should include(QUARTER, QUARTER, DIME, NICKEL)
-  end
-  
 end
