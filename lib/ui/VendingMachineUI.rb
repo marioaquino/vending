96d8ab316ed2c268b265f417e0c018e2c5872546
Mario Aquino <mario.e.aquino@gmail.com>
2009-06-04 09:17:54 -0500

    Updates to the MacRuby UI.

--- lib/ui/VendingMachineUI.rb
+++ lib/ui/VendingMachineUI.rb
@@ -7,58 +7,78 @@
 #
 
 class VendingMachineUI
-  attr_accessor :table_view, :exact_change_button, :denomination_popup
-  attr_accessor :amount_deposited_text
+  attr_accessor :sale_items_view, :exact_change_button, :denomination_popup
+  attr_accessor :amount_deposited_text, :dispensary_bin
   
   def awakeFromNib
     @vending_machine = VendingMachine.new 'password'
     
-    #exact_change_button.setEnabled false
-	  
-    amount_deposited_text.setStringValue @vending_machine.money_deposited
-	  
 	  denomination_popup.removeAllItems
-	  
 	  denomination_titles = Money::denominations.values.sort.map {|value| Money::token_for(value).to_s.capitalize }
-	  
 	  denomination_popup.addItemsWithTitles denomination_titles
 	  
     prices = @vending_machine.sale_prices_by_column
     prices.each_with_index do |pair, index|
-      table_column = @table_view.tableColumns[index]
+      table_column = @sale_items_view.tableColumns[index]
       column_key = pair.first
       column_price = pair.last
-      table_column.setIdentifier(pair[0])
+      table_column.setIdentifier(column_key)
       table_column.headerCell.setStringValue "#{column_key} (#{column_price})"
     end
     
+    dispensary = @dispensary_bin.tableColumns[0]
+    dispensary.setIdentifier(:items)
+    dispensary.headerCell.setStringValue "Dispensary Bin"
+
+    # FIXME: This is only here temporarily until the service mode is hooked into the UI
   	@vending_machine.service 'password'
   	{a: :Doritos, b: :DingDongs, c: :MicrowaveHamburger}.each do |column, item|
   	  @vending_machine.stock column, *([item] * 3)
   	end
   	@vending_machine.activate_vending
   	
-    @table_view.dataSource = VendingTableItemsDataSource.new @vending_machine
+    @sale_items_view.dataSource = VendingTableItemsDataSource.new @vending_machine
+    @dispensary_bin.dataSource = DispensaryBinItemsDataSource.new @vending_machine
+    update_view
   end
   
   def column_a_selected(sender)
-    puts "Select A clicked"
+    make_selection :a
   end
   
   def column_b_selected(sender)
+    make_selection :b
   end
   
   def column_c_selected(sender)
+    make_selection :c
   end
   
   def deposit_money_clicked(sender)
     selected_denomination = denomination_popup.titleOfSelectedItem.upcase.to_sym
     value = Money.const_get selected_denomination
     @vending_machine.add_money value
-    amount_deposited_text.setStringValue @vending_machine.money_deposited
+    update_view
   end
   
   def cancel_sale_clicked(sender)
+  	@vending_machine.cancel
+  	update_view
+  end
+  
+  private
+  def update_view
+    amount_deposited_text.setStringValue @vending_machine.money_deposited
+    exact_change_needed = @vending_machine.exact_change_needed?
+    button_state = exact_change_needed ? NSOnState : NSOffState
+    exact_change_button.highlight exact_change_needed
+    exact_change_button.setState button_state
+  end
+  
+  def make_selection(column)
+    @vending_machine.make_selection column
+    [sale_items_view, dispensary_bin].each{|table| table.reloadData}
+    update_view
   end
 end
 
@@ -74,5 +94,19 @@ class VendingTableItemsDataSource
   def tableView(view, objectValueForTableColumn:column, row:index)
 	  items = @vending_machine.sale_items_by_column[column.identifier]	  
 	  items[index]
+  end
+end
+
+class DispensaryBinItemsDataSource
+  def initialize(vending_machine)
+    @vending_machine = vending_machine
+  end
+  
+  def numberOfRowsInTableView(view)
+    @vending_machine.dispensary.size
+  end
+  
+  def tableView(view, objectValueForTableColumn:column, row:index)
+    @vending_machine.dispensary[index]
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'spec'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'vending'
+require 'ui/notifier'
 
 QUARTER = Money::QUARTER
 NICKEL = Money::NICKEL
